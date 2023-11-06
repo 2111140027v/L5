fce5d125fff02615005545d3efe54767b89d5ac8
Haruto Nakaji <2111140027v@ed.fuk.kindai.ac.jp>
2023-11-06 08:52:49 +0000

    いいね機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_063441) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_080849) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.string "message"
     t.integer "user_id"
